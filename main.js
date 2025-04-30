057ce5c6808a35d19d7c4d3d0463f474e4e8f060
tga55a <tgardner2@my.waketech.edu>
2025-04-30 14:02:21 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,23 +3,39 @@ const addTask = document.getElementById("addTask")
 const inputField = document.getElementById("inputField")
 const TasksList = document.getElementById("TasksList")
 
-let GlobalTasks = [
+const Importance = document.getElementById("Importance")
+const Priority = document.getElementById("Priority")
 
-]
+let GlobalTasks = []
 
 
-function createTaskElement(taskName, deleteCallback) {
+function createTaskElement(taskName, priority, date, deleteCallback) {
 
     let group = document.createElement("div")
     let children = {
         "taskName": document.createElement("p"),
-        "delete": document.createElement("button"),
 
+        "isCompleteLabel": document.createElement("label"),
+        "isComplete": document.createElement("input"),
+        
+        "Priority": document.createElement("p"),
+
+        "Date": document.createElement("p"),
+
+        "delete": document.createElement("button"),
     }
 
     // Assign properties
     children.taskName.textContent = taskName
     children.delete.textContent = "Delete"
+
+    children.isCompleteLabel.htmlFor = "isComplete"
+    children.isCompleteLabel.textContent = "Done"
+    children.isComplete.id = "isComplete"
+    children.isComplete.type = "checkbox"
+
+    children.Priority.textContent = `Priority: ${priority}`
+    children.Date.textContent = `Date: ${date}`
 
     // Assign delete event
     children.delete.addEventListener("click", () => {
@@ -35,20 +51,17 @@ function createTaskElement(taskName, deleteCallback) {
     TasksList.appendChild(group)
 
 
-    return group
+    return {"group": group, "children": children}
 
 }
 function onAddTask() {
-    console.log('wassup bruh')
-
-    console.log(inputField.value)
     let newTask = new Task(
         GlobalTasks.length + 1,
-        "High",
+        Priority.value,
         inputField.value,
+        Importance.value,
         false,
-        false,
-        Date.now()
+        Date()
     )
 
     GlobalTasks.push(newTask)
@@ -65,17 +78,22 @@ class Task {
         this.isCompleted = isCompleted 
         this.date = date
         this.TaskElement = createTaskElement(
-            this.task, 
+            taskContent,
+            priority,
+            date,
             this.deleteTask() // Returns an inner function that depends on the "this" argument of the outer function (since "this" will transition from the Task instance to the HTML element)
         )
 
-        console.log(this.TaskElement)
+        let isComplete = this.TaskElement.children.isComplete
+        isComplete.addEventListener("click", () => {
+            this.isCompleted = !this.isCompleted
+            console.log(this.isCompleted)
+        })
     }
-
     deleteTask() {
-        let taskObject = this 
+        let taskObject = this
         return function () {
-            console.log(`delete ${taskObject.task}`)
+            TasksList.removeChild(taskObject.TaskElement.group)
         }
     }
 }
